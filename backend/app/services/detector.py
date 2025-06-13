b8eb1f3698fe85f14a804296592efae57f53cf9d
EliteHacker228 <mchappycraft@yandex.ru>
2025-06-14 02:59:32 +0500

    fix: fix none check for actions

--- backend/app/services/detector.py
+++ backend/app/services/detector.py
@@ -43,7 +43,7 @@ def rotate_image(image, angle):
 def process_image_file(image_path: str) -> str:
     image = cv2.imread(image_path)
     if image is None:
-        raise FileNotFoundError(f"File not found or cannot be read: {filepath}")
+        raise FileNotFoundError(f"File not found or cannot be read: {image_path}")
 
     results = model(image)
 
