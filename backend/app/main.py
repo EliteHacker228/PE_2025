2e98b60a8cd37887a1e0c71babb07b7d6714a2a6
gribnik-s-gribom <iura.ereminnn@gmail.com>
2025-06-14 13:03:07 +0500

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,16 +1,16 @@
 from fastapi import FastAPI, UploadFile, File, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
+import os
+
 from app.api.endpoints import router as api_router
 from app.services.detector import process_image_file
-import os  # Добавьте импорт os
+
 
 app = FastAPI(
     title="Table Detector Service",
     description="Сервис определения таблиц и корректировки поворота сканов",
     version="1.0.0",
 )
-
-
 
 app.add_middleware(
     CORSMiddleware,
